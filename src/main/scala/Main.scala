807808f2cde0be107af5601c42c6941a01d07dc3
stevebizimungu <bizimungu.steve@gmail.com>
2023-03-06 16:11:51 +0100

    adding principals

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,16 +11,22 @@ object Main extends App {
 
   def runPrincipalsForMovieName(): Unit = {
 
+    //Testing value
     //Some movie names
     //Surviving the Social
     //Blood Money	Blood Money
 
+    //Implementation A
     MovieServiceImplA.principalsForMovieName("Blacksmith Scene")
+
+    //Implement B
+    MovieServiceImplB.principalsForMovieName("Blacksmith Scene")
       .runForeach(println)
       .onComplete(_ => system.terminate())
   }
 
   private def runTvSeriesWithGreatestNumberOfEpisodes(): Unit = {
+    //MovieServiceImplA.tvSeriesWithGreatestNumberOfEpisodes()
     MovieServiceImplB.tvSeriesWithGreatestNumberOfEpisodes()
       .runForeach(println)
       .onComplete(_ => system.terminate())
